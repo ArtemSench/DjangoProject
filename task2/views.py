c59bdab2b2e10d4cf9b3889600d84e9eb6fb36e9
ArtemSench <161152828+ArtemSench@users.noreply.github.com>
2024-09-09 10:48:06 +0300

    class перенес во views.py

--- task2/views.py
+++ task2/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 def main(request):
     return render(request, 'main.html')
 def func_template(request):
     return render(request, 'func_template.html')
-class class_template
+class class_template(TemplateView)
+    template_name = 'class_template.html'
